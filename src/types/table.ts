5346ef0d1a7c9536f2a48d1473a99c3b9babeef4
dyi919 <ehddjs13579@gmail.com>
2025-04-08 00:57:57 +0900

    chore: add FilterFunction type

--- src/types/table.ts
+++ src/types/table.ts
@@ -1,7 +1,12 @@
+import { TABLE_FILTER_FUNCTION } from '@/constants/table';
+
+export type FilterFunction =
+  (typeof TABLE_FILTER_FUNCTION)[keyof typeof TABLE_FILTER_FUNCTION];
+
 export type Column = {
   accessorKey: string;
   header: string;
-  filterFn: string;
+  filterFn: FilterFunction;
 };
 
 export type TableRowData = {
